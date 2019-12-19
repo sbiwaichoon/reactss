733ae3cb3c3baabff6e94ca68e998a3b1e3f2051
Wai Choon <waichoon@seanbusinessinternational.com>
2019-12-19 09:11:49 +0800

    set ip

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-// export const pubApi ='http://localhost:8888/sbi/v2/public_core/';
-export const pubApi ='http://localhost.192.168.31.34.xip.io:8888/sbi/v2/public_core/';
-export const pubImageApi ='http://localhost.192.168.31.34.xip.io:8888/sbi/media';
+const ipAddress = '192.168.31.34';
+export const pubApi =`http://localhost.${ipAddress}.xip.io:8888/sbi/v2/public_core/`;
+export const pubImageApi =`http://localhost.${ipAddress}.xip.io:8888/sbi/media`;
 //http://localhost.192.168.0.114.xip.io:8888
