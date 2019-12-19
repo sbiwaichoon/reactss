dc0cd28f208faeb739e69be752fa5dedcd6b3500
karynkh <khaihui@seanbusinessinternational.com>
2019-12-19 09:29:38 +0800

    add graph

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View,StyleSheet,ScrollView, Text} from 'react-native';
+import { View,StyleSheet,ScrollView, Text,Dimensions} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 // import {Button,ButtonLink} from '../components/Button';
 import {Container} from '../components/Container'
@@ -15,6 +15,19 @@ import { setNickName } from '../actions/loginActions';
 import { setpage,userLogout } from '../actions/navActions';
 import LinearGradient from 'react-native-linear-gradient';
 import ElevatedView from 'react-native-elevated-view'
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
+import * as Progress from 'react-native-progress';
+import {Surface, Shape} from '@react-native-community/art';
+
+const screenWidth = Dimensions.get("window").width;
+
 
 class home extends Component {
   constructor(props) {
@@ -62,6 +75,7 @@ class home extends Component {
                 <Text style={styles.Headertxth1}>Good Morning</Text>
                 <Text style={styles.Headertxt}>Don't forget to check in</Text>
             </View>
+            <View style={styles.punchborder}>
             <LinearGradient 
             start={{x: 0.0, y: 1}} 
             end={{x: 1, y: 0.8}}
@@ -73,6 +87,7 @@ class home extends Component {
                     <Text style={styles.punchtime}>08.30 AM</Text>
                 </View>
             </LinearGradient>
+            </View>
           </ElevatedView>
         
 
@@ -83,6 +98,32 @@ class home extends Component {
               style={styles.stayElevated}>
 
               <Text style={styles.title}>Target</Text>
+              <View>
+                <BarChart width={150} height={200}
+              yAxisLabel={'$'} verticalLabelRotation={90}
+              data={{
+                
+                datasets: [
+                  {
+                    data: [20, 45, 28, 80, 99, 43]
+                  }
+                ]
+              }}
+              chartConfig={{
+                backgroundGradientFrom: '#667bce',
+                backgroundGradientFromOpacity: 0,
+                backgroundGradientTo: "#08130D",
+                backgroundGradientToOpacity: 0,
+                color: (opacity = 1) => `rgba(85, 70, 226, ${opacity})`,
+                strokeWidth: 2, // optional, default 3
+                barPercentage: 0.4
+              }}
+              style={{
+                justifyContent:'center',
+                alignItems: 'center',
+              }}
+               />
+              </View>
               </ElevatedView>
             </View>
 
@@ -92,6 +133,9 @@ class home extends Component {
                 elevation={4}
                 style={styles.stayElevated}>
                 <Text style={styles.title}>Performance</Text>
+                <View>
+                <Progress.Circle size={30} indeterminate={true} />
+                </View>
               </ElevatedView>
             </View>
           </View>
@@ -101,8 +145,8 @@ class home extends Component {
               <ElevatedView
               elevation={4}
               style={styles.stayElevated}>
-
               <Text style={styles.title}>Income</Text>
+              
               </ElevatedView>
             </View>
 
@@ -112,6 +156,19 @@ class home extends Component {
                 elevation={4}
                 style={styles.stayElevated}>
                 <Text style={styles.title}>Task</Text>
+                <View style={styles.graphCont}>
+                  <Text style={{fontSize:40,fontWeight:'bold',paddingBottom:25,paddingTop:25}}>3/5</Text>
+                </View>
+                <View>
+                  <Text style={{fontSize:12,paddingHorizontal:15,paddingBottom:5}}>Personal Requirement</Text>
+                  <View style={{justifyContent: 'center',alignItems: 'center',}}>
+                  <Progress.Bar progress={0.5} width={150} />
+                  </View>
+                  <Text style={{fontSize:12,paddingHorizontal:15,paddingTop:10,paddingBottom:5}}>Promotion for Product A</Text>
+                  <View style={{justifyContent: 'center',alignItems: 'center',}}>
+                  <Progress.Bar progress={0.8} width={150} />
+                  </View>
+                </View>
               </ElevatedView>
             </View>
           </View>
@@ -136,7 +193,6 @@ function matchDispatchToProps(dispatch) {
   return bindActionCreators({  setpage: setpage,setNickName:setNickName,userLogout:userLogout }, dispatch)
 }
 export default connect(mapStateToProps, matchDispatchToProps)(home);
-
 
 
 const styles = StyleSheet.create({
@@ -166,13 +222,18 @@ Headertxt:{
     color:'#858587',
     paddingTop:2,
 },
+punchborder:{
+  borderColor:'#deddd9',
+  borderWidth:2,
+  borderRadius:100,
+},
 punchCont:{
     flex:1,
     width:90,
     height:90,
-    resizeMode:'cover',
+    margin:3,
     justifyContent:'center',
-    borderRadius:100,
+    borderRadius:50,
     shadowColor: 'rgba(235,235,235, 1)',
     shadowOffset: { width: 0, height: 3 },
     shadowOpacity: 0.9,
@@ -220,5 +281,9 @@ stayElevated: {
   width: '100%',
   height: '100%',
   backgroundColor: 'white',
-}
+},
+graphCont:{
+  justifyContent:'center',
+  alignItems: 'center',
+},
 });
